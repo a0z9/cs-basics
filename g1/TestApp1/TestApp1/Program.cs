bc4092adf1db935813b944d85af6e04387b4d021
Andres <a0z9@rambler.ru>
2023-11-21 11:38:00 +0300

    testApp

---
+++ g1/TestApp1/TestApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using hello.krasn2023.learning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace krasn2023.learning
+{   class Sfu
+    {
+
+        static void SayHello(string name)
+        {
+            System.Console.WriteLine($"Salute! Салют, {name}!");
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Hi, Sfu!!");
+            
+            Write("Enter Name: "); var name = ReadLine();
+
+            WriteLine($"Hi, {name}!");
+            WriteLine("Hi, {0}!",name);
+            string output = String.Format("Привет, {0}", name);
+            WriteLine(output);
+            Hello.SayHello(name);
+            SayHello(name);
+
+
+
+
+        }
+    }
+}
