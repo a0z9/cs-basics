cc69c3e23656db99f22ba4452ff1ac7a5f807b45
Andres <a0z9@rambler.ru>
2023-11-21 15:01:04 +0300

    start-dot-net

---
+++ g2/TestApp1/TestApp1/Hello.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace hello.krasn2023.sfu.training
+{
+    internal class Hello
+    {
+        public static void SayGreets(bool ask = false, string name = "Andrei")
+        {
+            WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
+            WriteLine("Hi, SFU!");
+
+            if (ask)
+            { Write("Введите имя: "); name = ReadLine(); }
+
+            WriteLine($"Привет, {name}!");
+            WriteLine("How do you do, " + name + "!");
+            var greets = "Как дела, " + name + "!";
+            WriteLine(greets);
+            greets = String.Format("Салют, {0}!", name);
+            WriteLine(greets);
+            WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
+        }
+
+    }
+}
---
+++ g2/TestApp1/TestApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace krasn2023.sfu.training
+{
+    internal class Sfu
+    {
+        private static void SayGreets(bool ask = false, string name = "Andrei") {
+            WriteLine("Hi, SFU!");
+
+            if (ask)
+            { Write("Введите имя: "); name = ReadLine(); }
+
+            WriteLine($"Привет, {name}!");
+            WriteLine("How do you do, " + name + "!");
+            var greets = "Как дела, " + name + "!";
+            WriteLine(greets);
+            greets = String.Format("Салют, {0}!", name);
+            WriteLine(greets);
+        } 
+
+        static void Main(string[] args)
+        {
+            SayGreets();
+            SayGreets(true);
+
+            ReadLine();
+
+
+        }
+    }
+}
