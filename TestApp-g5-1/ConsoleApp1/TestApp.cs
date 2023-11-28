cb2a1a6d1a7c640355389ac053bdefd7043cf254
Andres <a0z9@rambler.ru>
2023-11-28 15:47:43 +0300

    HelloClasses

---
+++ TestApp-g5-1/ConsoleApp1/TestApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace krasn23.a0z9.learning
+{
+    internal class TestApp
+    {
+        void SayHello(string name) {
+            
+            WriteLine($"HowDoUDo, {name}!");
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("KrasnYarsk 2023 c# learning base.Hi!");
+            Write("Enter Name: ");
+            string name = Console.ReadLine();
+            
+            WriteLine("Привет, " + name + "!");
+            string message = $"Привет, {name}!";
+            WriteLine(message);
+            string message2 = String.Format("Salute, {0}!!", name);
+            WriteLine("Привет, {0}!",name);
+            WriteLine(message2);
+            WriteLine("---------- Say Hello Func ---------");
+            new TestApp().SayHello(name);
+
+            ReadKey();
+        }
+    }
+}
