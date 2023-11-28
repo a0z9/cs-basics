fc31a47a8af8c39393717b00b0f6fe09661a1826
Andres <a0z9@rambler.ru>
2023-11-28 13:42:59 +0300

    HelloClasses

--- TestApp1/ConsoleApp1/TestVariables.cs
+++ TestApp1/ConsoleApp1/TestVariables.cs
@@ -14,21 +14,37 @@ namespace krasn23.a0z9.learning
         {
             WriteLine($"Салют, {name}!");
         }
+        static void HelloTest() {
 
-        static void Main(string[] args)
-        {
             WriteLine("G3 c# training! Hi all!");
-            Write("Ваше имя: "); 
+            Write("Ваше имя: ");
             string name = ReadLine();
 
             WriteLine("Привет, " + name + "!!");
             WriteLine($"Привет, {name}!!");
-            WriteLine("Привет, {0}!!",name);
-            
+            WriteLine("Привет, {0}!!", name);
+
             SayHello(name);
             Hello.SayHello(name);
             Hello2.SayHello(name);
+        }
+        static void Main(string[] args)
+        {
+            // HelloTest();
 
+            //------------ Numbers ------------------
+            WriteLine("------------ Numbers ------------------");
+            byte b1 =  unchecked((byte)1123);
+            WriteLine($"{b1} -- {Convert.ToString(b1, 2)}");
+            sbyte sb1 = 121, sb2 = 6;
+            sbyte sb3;
+
+         //   checked
+         //   {
+                sb3 = (sbyte)(sb1 + sb2);
+         //   }
+
+            WriteLine($"{sb3} -- {Convert.ToString(sb3, 2)}");
 
 
             ReadKey();
