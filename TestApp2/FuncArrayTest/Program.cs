d8ca894c3f2d0dc0cdccbc34ae440f08cdc5b543
Andres <a0z9@rambler.ru>
2023-11-29 13:37:43 +0300

    arrays,funcs

--- TestApp2/FuncArrayTest/Program.cs
+++ TestApp2/FuncArrayTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using static System.Console;
 
+
+
 static void LoopTest() {
 
     for (int i = 5, k = 1; i > -1 && k < 5; i -= 2, k++)
@@ -37,8 +39,37 @@ WriteLine("Func Array Test");
 { int i = 22;}
 { int i = 1222;}
 
-LoopTest();
+//LoopTest();
 
+int[] arr =  [ 1, 2, 3, -20, 4, -4 ]; // .net > 6
+
+int[] arr2 = new int[]{ 10, -2, 3, -20, 4, -4 };
+
+int[] arr3 = new int[10];
+
+arr[4] = 77;
+
+for (int i = 0; i < arr.Length; i++) WriteLine($"arr[{i}]={arr[i]}");
+WriteLine("--------------------------------------");
+Write("[");
+foreach (int d in arr) Write(d + ", ");
+if (arr.Length != 0) Write("\b\b");
+WriteLine ("]");
+
+WriteLine("------- Main Args --------------");
+foreach (string a in args) WriteLine(a);
+
+
+// ---------- switch/case
+
+LogSeverity ls =  LogSeverity.Middle;
+switch (ls)
+{
+    case LogSeverity.Info: WriteLine("Info"); break;
+    case LogSeverity.Middle: WriteLine("Middle"); break;
+    //....
+    default: WriteLine("Wrong Log!!"); break;
+}
 
 ReadKey();
 
@@ -58,4 +89,17 @@ static void Summator()
         { WriteLine($"{a1} + {a2} = {a1 + a2}"); }
         else { WriteLine("Wrong format!!!"); }
     }
+}
+
+
+enum LogSeverity
+{
+    NoLog, Info, Low, Middle, High
+}
+
+enum FileAccesMode
+{
+    Read = 0b0001,
+    Write =0b0010,
+    Execute = 0b0100
 }
