e79abce8601c60719735f202cbcd38233f9da7bb
Andres <a0z9@rambler.ru>
2023-11-29 11:35:59 +0300

    TestVars

--- TestApp1/ConsoleApp1/TestVariables.cs
+++ TestApp1/ConsoleApp1/TestVariables.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using hello.krasn23.a0z9.learning;
 using static System.Console;
@@ -30,6 +31,9 @@ namespace krasn23.a0z9.learning
         }
         static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture =
+                    System.Globalization.CultureInfo.InvariantCulture;
+            
             // HelloTest();
 
             //------------ Numbers ------------------
@@ -45,6 +49,83 @@ namespace krasn23.a0z9.learning
          //   }
 
             WriteLine($"{sb3} -- {Convert.ToString(sb3, 2)}");
+
+            short sh1 = 1234, sh2 = 0x1f_ff;
+            WriteLine($"{short.MaxValue,6}, {short.MinValue,6} ");
+            WriteLine($"{(1<<15)-1,6}, {-(1 << 15),6} ");
+            WriteLine("------------------------------");
+
+            int i1 = 1111111;
+
+            unchecked
+            {
+                WriteLine($"{int.MaxValue,10}, {int.MinValue,10} ");
+                WriteLine($"{(1 << 31) - 1,10}, {-(1 << 31),10} ");
+            }
+
+            // 0001_1100 
+            // << 1
+            // 0011_1000
+
+            unchecked
+            {
+                WriteLine($"{long.MaxValue,10}, {long.MinValue,10} ");
+                WriteLine($"{(1L << 63) - 1,10}, {-(1L << 63),10} ");
+                WriteLine($"{ulong.MaxValue,10}, {ulong.MinValue,10} ");
+            }
+
+            // --- floats ----
+
+            float f1 = 1234.5678901234567890f;  // 1.2345678901234567890e3
+            WriteLine("Float Precision:" + Math.Log10(1 << 23));
+            WriteLine($"f1={f1}");
+
+            //
+
+            // --- doubles ----
+
+            double d1 = 1234.5678901234567890;  // 1.2345678901234567890e3
+            WriteLine("Double Precision:" + Math.Log10(1L << 52));
+            WriteLine($"d1={d1}");
+
+            // --- decimal ----
+            WriteLine(" ------------ Decimal test  ----------------- ");
+            decimal dc = 1m / 3m;
+            double  dd = 1d / 3d;
+
+            WriteLine(dc);
+            WriteLine(dd);
+
+            dc = (0.3m + 0.3m + 0.3m)/0.2m;
+            dd = (0.3 + 0.3 + 0.3)/0.2;
+
+            WriteLine(dc);
+            WriteLine(dd);
+
+            //------------- Operators ----------------------
+
+            // ++, --, +=, -=, ... &,|, !, ^
+
+            dd = dd * 1.01; // dd *= 1.01;
+
+            int a1 = 0b1011_1100;
+            int a2 = 0b1100_1001;
+
+            WriteLine($"{"a1=",5} {Convert.ToString(a1,2),10}");
+            WriteLine($"{"a2=",5} {Convert.ToString(a2,2),10}");
+            WriteLine($"{"a1&a2",5} {Convert.ToString(a1 &a2,2),10}");
+            WriteLine($"{"a1|a2",5} {Convert.ToString(a1|a2,2),10}");
+            WriteLine($"{"a1^a2",5} {Convert.ToString(a1 ^a2,2).PadLeft(8,'0'),10}");
+
+
+
+
+
+
+
+
+
+
 
 
             ReadKey();
