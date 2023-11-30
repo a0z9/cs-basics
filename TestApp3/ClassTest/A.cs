f09be94af32a5be7a30d7483e89940f0c1448dea
Andres <a0z9@rambler.ru>
2023-11-30 16:51:05 +0300

    class-start

---
+++ TestApp3/ClassTest/A.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace krasn23.a0z9.learning
+{
+    internal class A
+    {
+        private int data;
+
+        public int Data
+        {
+            get { return data; }
+            set { 
+                if(value >=0)
+                data = value; }
+        }
+
+
+        public int GetData () => data;
+        public void SetData(int value) =>  data = value;
+
+        public A(int data) {
+            Data = data;
+            WriteLine("+++ ctor of A type, o:" + GetHashCode());
+        }
+
+        public A() : this(0) { }
+
+        public override string ToString()
+        {
+            return $"Class A, data={data}, ref={GetHashCode()}.";
+        }
+    }
+}
---
+++ TestApp3/ClassTest/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using static System.Console;
+
+namespace krasn23.a0z9.learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("-------- class test ----------");
+            A a = new A();
+            a.Data = 123;
+            WriteLine(a);
+
+            A b = a; b.Data = 7771111;
+            WriteLine(a);
+
+            WriteLine(new A(10));
+            WriteLine(new A() {Data = -9827987});
+            WriteLine(new A());
+
+
+            ReadKey();
+        }
+    }
+}
