dc1b384f71d1d73bdb77bca33823a3e704bde8e5
Andres <a0z9@rambler.ru>
2023-12-01 15:09:53 +0300

    ini

---
+++ TestApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace krasn23.a0z9.learning
+{
+    //krasn23.a0z9.learning.Program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(" --- Hi, c# learing! --- ");
+            WriteLine(" -- Привет, c# тренинг! --- ");
+
+            Write("Ваше имя: ");
+            string name = ReadLine();
+            WriteLine("Привет, " + name + "!");
+            
+        }
+    }
+}
