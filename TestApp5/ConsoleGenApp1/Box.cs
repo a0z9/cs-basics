4b3cde4865e70ecbe583e4dd4b401c392a55e741
Andres <a0z9@rambler.ru>
2023-12-06 19:12:08 +0300

    GenericTypes-Collections

--- TestApp5/ConsoleGenApp1/Box.cs
+++ TestApp5/ConsoleGenApp1/Box.cs
@@ -1,19 +1,64 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleGenApp1
 {
-    internal class Box<T>
+    internal class Box<T> : IEnumerable
     {
         private T[] _arr;
-        public uint N {  get; private set; }
+        private uint _N;
+        public uint N {  get => _N; private set => _N=value; }
         private uint _idx;
 
         public Box(uint N) { _arr = new T[N]; this.N = N; }
-        public Box(T[] arr) { _arr = arr; N = (uint)arr.Length; }
+        public Box(T[] arr) { _arr = arr; N = (uint)arr.Length; _idx = N; }
+
+        public Box<T> Add(T element)
+        {
+            //if (_idx >= N) ExposeArray(N * 2);
+            if (_idx >= N) ExposeArray2(ref _arr, N*2, out _N);
+            _arr[_idx++] = element;
+            return this;
+        }
+
+        private bool ExposeArray(uint N_new)
+        { 
+            if (N_new > N) { 
+                T[] _arr2 = new T[N_new];
+                Array.Copy(_arr, _arr2, N);
+                N = N_new;
+                _arr = _arr2;
+                return true; }
+
+            return false;
+        }
+
+         private static uint ExposeArray2(ref T[] arr,uint N_new, out uint N)
+        {
+            N = (uint)arr.Length;
+           
+            if (N_new > N)
+            {
+                T[] _arr2 = new T[N_new];
+                Array.Copy(arr, _arr2, N);
+                arr = _arr2;
+                N = N_new;
+                return N_new;
+            }
+            return N;
+           
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+           return _arr.GetEnumerator();
+        }
 
         public T this[uint idx] 
         {  get { return _arr[idx]; }
--- TestApp5/ConsoleGenApp1/Program.cs
+++ TestApp5/ConsoleGenApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace ConsoleGenApp1
 {
     internal class Program
     {
+        static double Summa<T>(T[] arr) //where T : class
+        { double s = 0;
+            foreach (object o in arr) s += Convert.ToDouble(o);
+          return s;
+        }
+
+
+        static double Summa2(params object[] arr) //where T : class
+        {
+            double s = 0;
+            foreach (object o in arr) s += Convert.ToDouble(o);
+            return s;
+        }
+
         static void Main(string[] args)
         {
             Element1 el = new Element1("aaa");
@@ -25,6 +40,23 @@ namespace ConsoleGenApp1
             Box<int> box1 = new Box<int>(3);
             Box<string> box2 = new Box<string>(new string[] {"aa","bb","cc"});
             for (uint i = 0; i < box2.N; i++) WriteLine(box2[i]);
+            box1.Add(111).Add(222).Add(333);
+            WriteLine("N=" + box1.N);
+            box1.Add(444).Add(555);
+            WriteLine("N=" + box1.N);
+            foreach (var v in box1) Write(v + ",");
+            WriteLine("\b");
+
+            for (uint i = 0; i < box1.N; i++) WriteLine(box1[i]);
+            foreach(var v in box2) WriteLine(v);
+            WriteLine("Summa1=" + Summa<int>(new int[] { 1, 2, 3, 4, 5 }));
+            WriteLine("Summa2=" + Summa<long>(new long[] { 1, 2, 3, 4, 5 }));
+            WriteLine("Summa3=" + Summa2(1, 2, 3, 4, 5 ));
+
+            int[] arr = new int[] {-1,4,2,-6,22,3, -6 };
+            WriteLine($"[{String.Join(",",arr)}]");
+            Array.Sort(arr, (a, b) => b - a);
+            WriteLine($"[{String.Join(",", arr)}]");
 
 
 
