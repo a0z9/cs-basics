30725e770b1897949c2daff5daf1197e88c1b9d2
Andres <a0z9@rambler.ru>
2023-12-06 11:02:21 +0300

    exceptions

---
+++ TestApp5/TestException/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TestException
+{
+    internal class Program
+    {
+        static void TestDiv(int a) {
+            int b = 1 / (a - 1);
+        }
+
+        static int getValue(uint idx)
+        {
+            int[] b = new int[] {1,2,-4,5,6 };
+            return b[idx];
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = getValue(5);
+                TestDiv(1);
+
+                WriteLine("-- End of Try Block --");
+            }
+            catch (DivideByZeroException ex)
+            {
+                // Log exception to file(s), log to network, ....
+                Console.WriteLine(ex + "\n------------------");
+                throw new Exception("Rethrow ex!!", ex);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                // Log exception to file(s), log to network, ....
+                Console.WriteLine(ex + "\n------------------");
+            }
+
+            finally
+            {
+                WriteLine("-- Finally Block --");
+            }
+
+
+            WriteLine("-- End of Main --");
+        }
+    }
+}
